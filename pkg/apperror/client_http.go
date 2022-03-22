432c9b2ef896d71d9db1be04918ad582d53cb69d
Sergey Shakotko <s.shakotko@city-mobil.ru>
2022-03-22 15:26:27 +0300

    small fix in error package

--- pkg/apperror/client_http.go
+++ pkg/apperror/client_http.go
@@ -3,7 +3,7 @@ package apperror
 import "net/http"
 
 func (err ClientError) GetHTTPCode() int {
-	return map[Code]int{
+	code := map[Code]int{
 		InvalidAuthorization: http.StatusUnauthorized,
 		InvalidParams:        http.StatusBadRequest,
 		PermissionDenied:     http.StatusForbidden,
@@ -14,4 +14,10 @@ func (err ClientError) GetHTTPCode() int {
 		Unavailable:          http.StatusServiceUnavailable,
 		Internal:             http.StatusInternalServerError,
 	}[err.Code]
+
+	if code <= 0 {
+		code = http.StatusInternalServerError
+	}
+
+	return code
 }
